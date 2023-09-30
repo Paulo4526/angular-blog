747c385bac674c587247efb6e19896e2752f86c9
Paulo Bueno <RM98527@fiap.com.br>
2023-09-30 14:47:33 -0300

    atualization blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { BigCardComponent } from './big-card/big-card.component';
+import { SmallCardComponent } from './small-card/small-card.component';
+import { TitleComponent } from './title/title.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    TitleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
